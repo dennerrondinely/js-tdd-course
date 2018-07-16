eb38115a7717e16c4b2059d6aec80e27f28140f1
Denenr <denner.silva@smartnx.io>
2018-07-16 12:51:21 -0300

    exercicios

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "eol-last": 0,
+        "linebreak-style": 0
+    }
+};
---
+++ src/main.js
@@ -0,0 +1,4 @@
+const a = 3;
+const b = 2;
+const c = 'adhgdkashdkjashdkhdshkasdhkjasdhkjasfhghgashdasdhgasjkdhasjdkgaskjdhasjkdhksjadhkhas';
+console.log(a, b, c);
